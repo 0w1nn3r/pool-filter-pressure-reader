aaa8f31b59858eb53dff36af1225b672b5a0f269
Owain Lloyd <119631+RocketD4n@users.noreply.github.com>
2025-06-14 20:44:37 +0200

    graph page refreshes every 5 seconds

--- src/WebServer.cpp
+++ src/WebServer.cpp
@@ -707,6 +707,7 @@ void WebServer::handlePressureHistory() {
     <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="refresh" content="5">
     <title>Pool Pressure History</title>
     <link rel='stylesheet' href='/style.css'>
     <style>
